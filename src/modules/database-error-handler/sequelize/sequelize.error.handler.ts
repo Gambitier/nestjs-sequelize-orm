657a0717ad3d8876013232f10cdd6e2cbcd1110b
gambitier <csegambitier@gmail.com>
2022-10-08 14:45:52 +0530

    add ForeignKeyConstraintFailedError

--- src/modules/database-error-handler/sequelize/sequelize.error.handler.ts
+++ src/modules/database-error-handler/sequelize/sequelize.error.handler.ts
@@ -1,7 +1,8 @@
 import { IDatabaseErrorHandler } from '@modules/database-error-handler/database.error.handler.interface';
 import { UniqueConstraintFailedError } from '@modules/database-error-handler/errors';
+import { ForeignKeyConstraintFailedError } from '@modules/database-error-handler/errors/foreignKey.constraint.failed.error';
 import { Injectable } from '@nestjs/common';
-import { UniqueConstraintError } from 'sequelize';
+import { ForeignKeyConstraintError, UniqueConstraintError } from 'sequelize';
 
 /////////////////////////////////////////////////////////////////////
 
@@ -16,6 +17,9 @@ export class SequelizeDatabaseErrorHandler implements IDatabaseErrorHandler {
       const fields = Object.keys(error.fields).join();
       const msg = `${fields} already in use`;
       throw new UniqueConstraintFailedError(fields, msg);
+    } else if (error instanceof ForeignKeyConstraintError) {
+      const msg = 'foreign key error occured';
+      throw new ForeignKeyConstraintFailedError('fields', msg);
     }
 
     throw error;
